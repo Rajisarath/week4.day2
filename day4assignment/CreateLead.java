0b41e5ae0d5988e8b45a86a5c80f20f26f965c5f
Rajisarath <116677132+Rajisarath@users.noreply.github.com>
2022-11-21 08:49:47 +0530

    Add files via upload

---
+++ day4assignment/CreateLead.java
@@ -0,0 +1,25 @@
+package w4.day4assignment;
+
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class CreateLead extends BaseClass{
+	
+    @Test
+	public void createLead() {
+		driver.findElement(By.linkText("Leads1")).click();
+		driver.findElement(By.linkText("Create Lead")).click();
+		driver.findElement(By.id("createLeadForm_companyName")).sendKeys("Testleaf");
+		driver.findElement(By.id("createLeadForm_firstName")).sendKeys("Subraja");
+		driver.findElement(By.id("createLeadForm_lastName")).sendKeys("Subi");
+		driver.findElement(By.id("createLeadForm_primaryPhoneNumber")).sendKeys("99");
+		driver.findElement(By.name("submitButton")).click();
+
+	}
+}
